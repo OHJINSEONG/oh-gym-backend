d6c222ad77300f5621d1cc59f7afc375c5984f79
OHJINSEONG <ojseong0828@naver.com>
2023-02-16 17:16:36 +0900

    트레이너 엔티티 create 추가
    어드민 트레이너 컨트롤러에 postmapping 추가
    
    트레이너 등록 Dto 추가
    
    트레이너 엔티티 create 구현

--- src/main/java/megatera/makaoGymbackEnd/services/AdminTrainerService.java
+++ src/main/java/megatera/makaoGymbackEnd/services/AdminTrainerService.java
@@ -2,11 +2,14 @@ package megatera.makaoGymbackEnd.services;
 
 import megatera.makaoGymbackEnd.dtos.DiaryResultDto;
 import megatera.makaoGymbackEnd.dtos.TrainerManagementDto;
+import megatera.makaoGymbackEnd.dtos.TrainerResultDto;
 import megatera.makaoGymbackEnd.dtos.UserDto;
-import megatera.makaoGymbackEnd.models.PtTicket;
+import megatera.makaoGymbackEnd.models.*;
+import megatera.makaoGymbackEnd.repositories.TrainerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +17,13 @@ import java.util.List;
 @Transactional
 public class AdminTrainerService {
     private final PtTicketService ptTicketService;
+    private final TrainerRepository trainerRepository;
     private final UserService userService;
     private final DiaryService diaryService;
 
-    public AdminTrainerService(PtTicketService ptTicketService, UserService userService, DiaryService diaryService) {
+    public AdminTrainerService(PtTicketService ptTicketService, TrainerRepository trainerRepository, UserService userService, DiaryService diaryService) {
         this.ptTicketService = ptTicketService;
+        this.trainerRepository = trainerRepository;
         this.userService = userService;
         this.diaryService = diaryService;
     }
@@ -31,9 +36,28 @@ public class AdminTrainerService {
         for (PtTicket ptTicket : ptTickets) {
             UserDto userDto = userService.find(ptTicket.userId());
             List<DiaryResultDto> diaryResultDtos = diaryService.list(ptTicket.userId());
-            trainerManagementDtos.add(new TrainerManagementDto(ptTicket.toDto(), userDto,diaryResultDtos));
+            trainerManagementDtos.add(new TrainerManagementDto(ptTicket.toDto(), userDto, diaryResultDtos));
         }
 
         return trainerManagementDtos;
+    }
+
+    public TrainerResultDto create(String name, String userName, String image) {
+        Trainer trainer = new Trainer(
+                new UserName(name),
+                new Name(userName),
+                new PhoneNumber("01085568965"),
+                new Age("971117-1932201"),
+                new Gender("남자"),
+                image,
+                LocalTime.parse("09:00"),
+                LocalTime.parse("18:00")
+        );
+
+        trainer.toCreated();
+
+        trainerRepository.save(trainer);
+
+        return trainer.toDto();
     }
 }
